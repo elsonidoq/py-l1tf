882ff8c67ae33f047cc80c515652230b9b413a17
elsonidoq <elsonidoq@gmail.com>
2014-07-18 16:29:20 -0300

    examples

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 py-l1tf
 =======
 
-Python implementation of L1 trend filtering algorithm
+Python implementation of L1 trend filtering algorithm using the [http://cvxopt.org/ cvxopt] library.
+The paper describing this technique can be found [http://web.stanford.edu/~gorin/papers/l1_trend_filter.pdf here]
+
+Here are some different fits for a timeseries downloaded from [http://www.barchartmarketdata.com/datasamples/US%20Futures%20Tick%20Query.csv here]
+
+[[File:l1tf/example/fits.png]]
+
--- l1tf/example/example.py
+++ l1tf/example/example.py
@@ -9,32 +9,32 @@ import pandas as pd
 with open('US Futures Tick Query.csv') as f:
     lines = list(csv.reader(f))
 
-x = pd.Series(np.asarray([float(e[-2]) for e in lines]))
+x = pd.Series(np.asarray([float(e[-2]) for e in lines[:1000]]))
 
-#plt.figure()
-#
-#for i, delta in enumerate([1, 10, 50, 100]):
-#    plt.subplot(2,2,i+1)
-#    filtered = l1tf(x, delta)
-#    plt.plot(x, label='Original data')
-#    plt.plot(filtered, linewidth=5, label='Filtered, delta=%s' % delta, alpha=0.5)
-#    plt.legend(loc='best')
-#
-# Adding outliers
-
-
-outliers_percentaje = 0.05
+outliers_percentaje = 0.2
 outliers = np.random.random(len(x)) < outliers_percentaje 
 
-x[outliers] = np.random.random(outliers.sum()) * 2 + x[outliers]
+x_w_outliers = x.copy()
+x_w_outliers[outliers] = (np.random.random(outliers.sum()) - 0.5) * 2 + x[outliers]
 
 plt.figure()
-
-for i, mad_factor in enumerate([1, 3, 10, 30]):
+plt.suptitle('Different fits by changing the $\delta$ parameter')
+for i, delta in enumerate([1, 3, 10, 30]):
     plt.subplot(2,2,i+1)
-    x_wo_outliers = remove_outliers(x, delta=1, mad_factor=mad_factor)
+    filtered = l1tf(x, delta)
     plt.plot(x, label='Original data')
-    plt.plot(x_wo_outliers, linewidth=5, label='Without outliers, mad_factor=%s' % mad_factor, alpha=0.5)
+    plt.plot(filtered, linewidth=5, label='Filtered, $\delta$ = %s' % delta, alpha=0.5)
+    plt.legend(loc='best')
+
+
+plt.figure()
+plt.suptitle('Outlier detection algorithm changing the mad_factor parameter')
+
+for i, mad_factor in enumerate([1, 3]):
+    plt.subplot(1,2,i+1)
+    x_wo_outliers = remove_outliers(x_w_outliers, delta=1, mad_factor=mad_factor)
+    plt.plot(x_w_outliers, label='Original data')
+    plt.plot(x_wo_outliers, linewidth=5, label='Without outliers, mad_factor = %s' % mad_factor, alpha=0.5)
     plt.legend(loc='best')
 
 plt.show()

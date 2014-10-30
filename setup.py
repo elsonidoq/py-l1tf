0aad2901528c307d7fb2554f86555fb3a53f4508
elsonidoq <elsonidoq@gmail.com>
2014-10-29 23:07:43 -0300

    fixed dependencies in setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,6 @@ setup(name='l1tf',
       url='pablozivic.com.ar',
       packages=['l1tf'],
       install_requires=['pandas==0.13.1',
-                'cvxopt==1.1.7',
-                'numpy==1.6.2',
-                'scikit-learn==0.14.1',
-                'matplotlib==1.1.1',
-                'mako==0.9.1',
-                'pymongo']
+                'cvxopt==1.1.7']
 
      )
